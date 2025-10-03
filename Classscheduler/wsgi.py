7cd647e78246e5ca1dd32d745a02873197427bda
Bank9101 <Bankaz123@gmail.com>
2025-10-03 18:29:08 +0700

    first commit

---
+++ Classscheduler/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for Classscheduler project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Classscheduler.settings')
+
+application = get_wsgi_application()
+
+app = application
---
+++ Planmate/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PlanmateConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Planmate'
